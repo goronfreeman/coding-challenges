4144d47a60e427c66958164df509ee9560ed11d8
Hunter Braun <hunter.braun@gmail.com>
2015-06-30 23:25:51 -0500

    complete fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+# Print the numbers 1..100
+# For multiples of 3, print "Fizz" instead of the number
+# For multiples of 5, print "Buzz" instead of the number
+# For multiples of 3 and 5, print "FizzBuzz" instead of the number
+
+@numbers = (1..100)
+
+@numbers.each do |i|
+  if i % 3 == 0 && i % 5 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'FizzBuzz'
+  else puts i
+  end
+end
