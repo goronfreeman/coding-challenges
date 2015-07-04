3cf1373346316fa2789d92975cb6069e3b17a92d
Hunter Braun <hunter.braun@gmail.com>
2015-07-04 16:20:08 -0500

    get user input

--- palindromic_numbers.rb
+++ palindromic_numbers.rb
@@ -1,12 +1,13 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/38yy9s/20150608_challenge_218_easy_making_numbers/
 
-a = 68
+print 'Enter an integer: '
+a = gets.chomp.to_i
 b = a
-count = 0
+counter = 0
 
-until b == b.to_s.reverse.to_i do
-  count += 1
+until b == b.to_s.reverse.to_i
+  counter += 1
   b = (b + b.to_s.reverse.to_i)
 end
 
-puts "#{a} gets palidromic after #{count} steps: #{b}"
+puts "#{a} gets palidromic after #{counter} steps: #{b}"
