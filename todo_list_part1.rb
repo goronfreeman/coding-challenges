c3957440660baff1c05b9e4c3dca359fe2c49835
Hunter Braun <hunter.braun@gmail.com>
2015-07-05 02:01:44 -0500

    complete todo list, part 1

---
+++ todo_list_part1.rb
@@ -0,0 +1,27 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/39ws1x/20150615_challenge_218_easy_todo_list_part_1/
+
+class TodoList
+  def initialize
+    @list = []
+    @count = 0
+  end
+
+  def add_item(task)
+    @list.push(task)
+  end
+
+  def delete_item(task)
+    @list.delete(task)
+  end
+
+  def view_list
+    if @list.empty?
+      puts 'Your todo list is empty.'
+    else
+      @list.each_with_index do |item, index|
+        index += 1
+        puts "#{index}) #{item}"
+      end
+    end
+  end
+end
