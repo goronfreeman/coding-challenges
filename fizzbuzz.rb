c8cac3d06edc85893e51ffa80733db0708beb2f7
Hunter Braun <hunter.braun@gmail.com>
2015-07-05 18:11:10 -0500

    fix typo

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 # For multiples of 5, print "Buzz" instead of the number
 # For multiples of 3 and 5, print "FizzBuzz" instead of the number
 
-@numbers = (1..100)
+numbers = (1..100)
 
-@numbers.each do |i|
+numbers.each do |i|
   if i % 3 == 0 && i % 5 == 0
     puts 'FizzBuzz'
   elsif i % 3 == 0
